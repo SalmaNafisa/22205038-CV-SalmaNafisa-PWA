8f62a5972c858358187bb1444a2dc70b598b5018
SALMA NAFISA QURROTU'AINI <145917431+SalmaNafisa@users.noreply.github.com>
2024-11-12 10:14:57 +0700

    Add files via upload

---
+++ assets/js/indexedDB.js
@@ -0,0 +1,76 @@
+let db;
+const request = indexedDB.open("db_komentar");
+let form = document.getElementById("form-indexed-db");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let nama = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let pesan = document.getElementById("pesan").value;
+
+  if (db) {
+    addData(db, nama, email, pesan);
+  } else {
+    console.error("Database belum siap.");
+  }
+});
+
+// // Buat database dan tabel
+request.onupgradeneeded = function (event) {
+  db = event.target.result;
+  let objectStore = db.createObjectStore("users", {
+    keyPath: "id",
+    autoIncrement: true,
+  });
+
+  objectStore.createIndex("nama", "nama", { unique: false });
+  objectStore.createIndex("email", "email", { unique: true });
+  objectStore.createIndex("pesan", "pesan", { unique: false });
+};
+
+// Handle error dan success saat membuka database
+request.onsuccess = (event) => {
+  db = event.target.result;
+  console.log(`Indexed DB - Successfully`);
+};
+
+request.onerror = (event) => {
+  console.error(`Oops error saat membuat indexedDB : ${event}`);
+};
+
+// Fungsi menambahkan data indexed DB
+function addData(db, nama, email, pesan) {
+  let transaction = db.transaction(["users"], "readwrite");
+  let objectStore = transaction.objectStore("users");
+
+  let user = { nama: nama, email: email, pesan: pesan };
+  let addRequest = objectStore.add(user);
+
+  addRequest.onsuccess = function () {
+    console.log("Data berhasil ditambahkan");
+    form.reset();
+    getData();
+  };
+}
+
+// Fungsi untuk mengambil dan menampilkan data Indexed DB
+function getData() {
+  let transaction = db.transaction(["users"], "readonly");
+  let object_store = transaction.objectStore("users");
+  let requestObjStore = object_store.openCursor();
+
+  requestObjStore.onerror = function (event) {
+    console.error(`Error fetching data : ${event}`);
+  };
+
+  requestObjStore.onsuccess = function (event) {
+    let cursor = event.target.result;
+    if (cursor) {
+      let key = cursor.primaryKey;
+      let value = cursor.value;
+      console.log(key, value);
+      cursor.continue();
+    }
+  };
+}
---
+++ service-worker.js
@@ -0,0 +1,106 @@
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here. Other Firebase libraries
+// are not available in the service worker.
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
+);
+
+const CACHE_NAME = "CV Salma";
+const urlsToCache = [
+  "/",
+  "/assets/css/style.css",
+  "/assets/css/swiper-bundle.min.css",
+  "/assets/js/main.js",
+  "/assets/js/swiper-bundle.min.js",
+  "/assets/img/icon-192x192.png",
+  "/assets/img/icon-512x512.png",
+  "/assets/img/about.jpg",
+  "/assets/img/favicon_edited.png",
+  "/assets/img/informatika.jpg",
+  "/assets/img/jepang.png",
+  "/assets/img/salma.png",
+  "/assets/img/shopee.png",
+  "/assets/img/tiktok.png",
+];
+
+// Install event: PWA
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => {
+        return self.skipWaiting();
+      })
+      .catch((error) => {
+        console.error("Failed to cache:", error);
+      })
+  );
+});
+
+// Activate event: hapus cache lama
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cache) => {
+            if (cache !== CACHE_NAME) {
+              return caches.delete(cache);
+            }
+          })
+        );
+      })
+      .then(() => {
+        return self.clients.claim();
+      })
+      .catch((error) => {
+        console.error("Failed to activate:", error);
+      })
+  );
+});
+
+// Fetch event: static assets
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+  apiKey: "AIzaSyBC2tWFeQqhQfkY_VOna1xdAhwrGVxDafs",
+  authDomain: "portofoliosall.firebaseapp.com",
+  projectId: "portofoliosall",
+  storageBucket: "portofoliosall.firebasestorage.app",
+  messagingSenderId: "227116829534",
+  appId: "1:227116829534:web:0481736d12e2f828f81dcf",
+  measurementId: "G-ELDYJMRH20",
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log(
+    "[firebase-messaging-sw.js] Received background message ",
+    payload
+  );
+  // Customize notification here
+  const notificationTitle = "Website Portofolio";
+  const notificationOptions = {
+    body: "Website Portofolio Salma",
+    icon: "/assets/img/icon background.jpg",
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
